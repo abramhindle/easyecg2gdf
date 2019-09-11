b0073b1f268de4659d037e79fe434aae311b7d12
Abram Hindle <abram.hindle@softwareprocess.es>
2019-09-11 11:19:50 -0600

    remove README.txt requirement

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+LICENSE
+=======
+
+This software is Free Software (GPLv2)
+
+Copyright (C) 2000-2014 George B. Moody and Edna S. Moody
+Copyright (C) 2017 Peter Lawrence
+Copyright (C) 2019 Abram Hindle
+
+Peter claims:
+
 tool to convert Easy ECG device data into open biosig format 
 ============================================================
 
@@ -19,3 +30,12 @@ It does hopefully translate the data into a .GDF file in the hopes that others h
 
 would result in all recordings found on the device (the specified directory) being written to the current directory as .GDF files, each with the date and time encoded into the filename.
 
+Abram: But I did not like at all.
+
+## Modifications
+
+I ignore README.txt and just allow you to process individual scp files
+
+./easyecg2gdf 1.SCP produces and .gdf file
+
+I did a bad job, a hack job you might say.
--- easyecg2gdf.c
+++ easyecg2gdf.c
@@ -84,62 +84,10 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 	{
-		fprintf(stderr, "%s <easyecg_path>\n", argv[0]);
+		fprintf(stderr, "%s ecgfile.scp\n", argv[0]);
 		return -1;
 	}
-
-	/*
-	The README.TXT file is a machine-parseable file masquerading as a human-readable text file.
-	It contains the first and last record numbers of every recording on the devices.
-	We open this first to know what records to read.
-	*/
-
-	snprintf(filename, sizeof(filename), "%sREADME.TXT", argv[1]);
-
-	readme = fopen(filename, "rb");
-
-	if (NULL == readme)
-	{
-		fprintf(stderr, "ERROR: unable to open index file (%s)\n", filename);
-		return -1;
-	}
-
-	/* we read README.TXT line-by-line looking for records */
-
-	while (fread(filename, 1, 32, readme))
-	{
-		/* if the line starts with a space, we've reached the end of the file */
-		if (0x20 == filename[0])
-			break;
-
-		/* each line is always 32-bytes; we must manually null-terminate the string */
-		filename[32] = '\0';
-
-		/* if ".scp" isn't in the line, it does not describe records */
-		if (!strstr(filename, ".scp"))
-			continue;
-
-		/* these are magic numbers to pull the first and last record numbers */
-		filename[12] = '\0';
-		filename[24] = '\0';
-		first_record = atoi(filename + 12 - 8);
-		last_record = atoi(filename + 24 - 8);
-
-		/* perform a sanity check on what we've decoded */
-		if ( (first_record < 1) || (first_record > 1200) || (last_record < 1) || (last_record > 1200) || (last_record < first_record) )
-		{
-			fprintf(stderr, "ERROR: scraping README.TXT yielded invalid records %d:%d\n", first_record, last_record);
-			return -1;
-		}
-
-		/* iterate through the records one-by-one from the first to the last */
-
-		for (current_record = first_record; current_record <= last_record; current_record++)
-		{
-			/* translate the record number into the subdirectory and filename that EasyECG uses */
-			snprintf(filename, sizeof(filename), "%s%s%d.SCP", argv[1], subdir[(current_record - 1) / 300], current_record);
-
-			/* read the file into memory */
+	strncpy(filename, argv[1], 256);
 
 			input = fopen(filename, "rb");
 
@@ -162,7 +110,8 @@ int main(int argc, char *argv[])
 
 			/* if this is the first record, then we need to open the output file and write the GDF header to it */
 
-			if (current_record == first_record)
+			/* if (current_record == first_record) */
+			if (1) /* only 1 file */
 			{
 				snprintf(filename, sizeof(filename), "%04d%02d%02d-%02d%02d%02d.GDF", ctx.date.year, ctx.date.month, ctx.date.day, ctx.time.hour, ctx.time.minute, ctx.time.second);
 
@@ -176,7 +125,7 @@ int main(int argc, char *argv[])
 
 				fprintf(stderr, "writing %s\n", filename);
 
-				gdf_parms.data_points = (ctx.datalen / 2) * (1 + last_record - first_record);
+				gdf_parms.data_points = (ctx.datalen / 2) /* * (1 + last_record - first_record) */;
 				gdf_parms.sample_rate_hz = 150;
 				gdf_parms.data_type = GDFTYP_float32;
 				gdf_parms.max = 2047.0 * ctx.amplitude / 1000000.0;
@@ -187,11 +136,9 @@ int main(int argc, char *argv[])
 
 			/* for every record, we write the data */
 			write_data(output, &ctx);
-		}
-	}
 
 	/* we're done, so close the two open files */
-	fclose(readme);
+	/* fclose(readme); */
 	fclose(output);
 
 	return 0;
